09080d4a0c8f25c6b48e0d76537c34b9afec4eec
Will Daly <will@edx.org>
2013-08-12 13:08:05 -0400

    Log warnings for missing files ONCE, not multiple times. Print log level with the log message.

--- js_test_tool/__init__.py
+++ js_test_tool/__init__.py
@@ -7,4 +7,5 @@ import logging
 
 CONSOLE_HANDLER = logging.StreamHandler()
 CONSOLE_HANDLER.setLevel(logging.WARNING)
+CONSOLE_HANDLER.setFormatter(logging.Formatter(fmt='%(levelname)s: %(message)s'))
 logging.getLogger('').addHandler(CONSOLE_HANDLER)
