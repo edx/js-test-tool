61e9a7e4829a085f3b00d49b0a9094b07ef1dfc6
Will Daly <will@edx.org>
2013-08-12 12:47:28 -0400

    Removed log file

--- js_test_tool/__init__.py
+++ js_test_tool/__init__.py
@@ -3,10 +3,7 @@ Perform global initialization.
 """
 
 # Configure the logger
-# All messages are logged a log file,
-# but warnings and above are also logged to stdout
 import logging
-logging.basicConfig(filename="js_test_tool.log", level=logging.DEBUG)
 
 CONSOLE_HANDLER = logging.StreamHandler()
 CONSOLE_HANDLER.setLevel(logging.WARNING)
